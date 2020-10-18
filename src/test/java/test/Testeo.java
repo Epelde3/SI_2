6eb86ed697c5931ab915a310d2ba25799f885acf
Haritz Epelde <haritzepelde@gmail.com>
2020-10-19 00:21:37 +0200

    proba travis

--- src/test/java/test/Testeo.java
+++ src/test/java/test/Testeo.java
@@ -3,7 +3,7 @@ package test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-//import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;
 //import org.mockito.InjectMocks;
 //import org.mockito.Mock;
 //import org.mockito.Mockito;
@@ -17,17 +17,17 @@ import service.*;
 //@RunWith(MockitoJUnitRunner.Silent.class)
 public class Testeo {
 	
-	//@Mock
+//	@Mock
 	DataAccessInterface dao;
 	
-	//@InjectMocks
+//	@InjectMocks
 	BLFacadeImplementation sut;
 
 	@Test
 	public void test() {
-		//Mocito lortu behar da
-		//Mockito.doReturn(null).when(dao).getBezeroa(Mockito.anyInt());
-		//assertTrue(dao.getBezeroa(0)==null);
+//		Mocito lortu behar da
+//		Mockito.doReturn(null).when(dao).getBezeroa(Mockito.anyInt());
+//		assertTrue(dao.getBezeroa(0)==null);
 		fail();
 		
 	}
