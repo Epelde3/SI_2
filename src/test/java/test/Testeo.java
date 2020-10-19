df067e5123b2040d374117c0035176433b1a3e03
Haritz Epelde <haritzepelde@gmail.com>
2020-10-19 14:33:14 +0200

    sonarcloud mockito funtzonatzen du

--- src/test/java/test/Testeo.java
+++ src/test/java/test/Testeo.java
@@ -2,32 +2,50 @@ package test;
 
 import static org.junit.Assert.*;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+
+
+
 import org.junit.Test;
+
+
+import dataAccess.DataAccess;
+
+import service.BLFacadeImplementation;
+
 import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+
+import org.mockito.runners.MockitoJUnitRunner;
+
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 
 import dataAccess.*;
 import service.*;
 
 
 
-//@RunWith(MockitoJUnitRunner.Silent.class)
+@RunWith(MockitoJUnitRunner.class)
 public class Testeo {
 	
-//	@Mock
-	DataAccessInterface dao;
+	DataAccess dao=Mockito.mock(DataAccess.class);
 	
-//	@InjectMocks
+	@InjectMocks
 	BLFacadeImplementation sut;
 
 	@Test
 	public void test() {
 //		Mocito lortu behar da
-//		Mockito.doReturn(null).when(dao).getBezeroa(Mockito.anyInt());
-//		assertTrue(dao.getBezeroa(0)==null);
+		Mockito.doReturn(null).when(dao).getBezeroa(Mockito.anyInt());
+		assertTrue(dao.getBezeroa(0)==null);
 		fail();
 		
 	}
